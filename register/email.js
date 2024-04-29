eb872a14cdb484e775b3c5081a422e99133619e7
SYED KIFAYAT UR RAHMAN <112537272+SyedKifayat1@users.noreply.github.com>
2024-04-29 06:15:43 +0500

    Add files via upload

---
+++ register/email.js
@@ -0,0 +1,102 @@
+function sendMail() {
+    console.log("Hi I am here");
+
+    var params = {
+        leadName: document.getElementById("name").value ? document.getElementById("name").value : "Name",
+        email: document.getElementById("email").value ? document.getElementById("email").value : "abc@gmail.com",
+        message: document.getElementById("message").value ? document.getElementById("message").value : "Message",
+        subject: document.getElementById("subject").value ? document.getElementById("subject").value : "Subject",
+        cnic: document.getElementById("cnic").value ? document.getElementById("cnic").value : "00000-000000-0",
+        contactNo: document.getElementById("contact").value ? document.getElementById("contact").value : "03000000000",
+        instituteName: document.getElementById("institute").value ? document.getElementById("institute").value : "Institute",
+        teamName: document.getElementById("team_name").value ? document.getElementById("team_name").value : "Team-Name",
+        referenceCode: document.getElementById("reference_code").value ? document.getElementById("reference_code").value : "Reference Code",
+        gammerTag: document.getElementById("leader_tag").value ? document.getElementById("leader_tag").value : "Leader Tag",
+        roboWeight: document.getElementById("dimension").value ? document.getElementById("dimension").value : "Dimension",
+        roboWeapon: document.getElementById("weapon").value ? document.getElementById("weapon").value : "Weapon",
+        member1Name: document.getElementById("member_name_1").value ? document.getElementById("member_name_1").value : "Member 1 Name",
+        member1Cnic: document.getElementById("member_cnic_1").value ? document.getElementById("member_cnic_1").value : "Member 1 CNIC",
+        member1Institute: document.getElementById("member_institute_1").value ? document.getElementById("member_institute_1").value : "Member 1 Institute",
+        member1GammerTag: document.getElementById("member_tag_1").value ? document.getElementById("member_tag_1").value : "Member Tag 1",
+        member2Name: document.getElementById("member_name_2").value ? document.getElementById("member_name_2").value : "Member Name 2",
+        member2Cnic: document.getElementById("member_cnic_2").value ? document.getElementById("member_cnic_2").value : "Member 2 CNIC ",
+        member2Institute: document.getElementById("member_institute_2").value ? document.getElementById("member_institute_2").value : "Member 2 Institute",
+        member2GammerTag: document.getElementById("member_tag_2").value ? document.getElementById("member_tag_2").value : "Member tag 2",
+        member3Name: document.getElementById("member_name_3").value ? document.getElementById("member_name_3").value : "Member 3 Name",
+        member3Cnic: document.getElementById("member_cnic_3").value ? document.getElementById("member_cnic_3").value : "Member 3 CNIC",
+        member3Institute: document.getElementById("member_institute_3").value ? document.getElementById("member_institute_3").value : "Member 3 Institute",
+        member3GammerTag: document.getElementById("member_tag_3").value ? document.getElementById("member_tag_3").value : "Member tag 3",
+        member4Name: document.getElementById("member_name_4").value ? document.getElementById("member_name_4").value : "Member 4 Name",
+        member4Cnic: document.getElementById("member_cnic_4").value ? document.getElementById("member_cnic_4").value : "Member 4 CNIC",
+        member4Institute: document.getElementById("member_institute_4").value ? document.getElementById("member_institute_4").value : "Member 4 Institute",
+        member4GammerTag: document.getElementById("member_tag_4").value ? document.getElementById("member_tag_4").value : "Member tag 4",
+        member5Name: document.getElementById("member_name_5").value ? document.getElementById("member_name_5").value : "Member 5 Name",
+        member5Cnic: document.getElementById("member_cnic_5").value ? document.getElementById("member_cnic_5").value : "Member 5 CNIC",
+        member5Institute: document.getElementById("member_institute_5").value ? document.getElementById("member_institute_5").value : "Member 5 Institute",
+        member6Name: document.getElementById("member_name_6").value ? document.getElementById("member_name_6").value : "Member 6 Name",
+        member6Cnic: document.getElementById("member_cnic_6").value ? document.getElementById("member_cnic_6").value : "Member 6 CNIC",
+        member6Institute: document.getElementById("member_institute_6").value ? document.getElementById("member_institute_6").value : "Member 6 Institute",
+        member7Name: document.getElementById("member_name_7").value ? document.getElementById("member_name_7").value : "Member 7 Name",
+        member7Cnic: document.getElementById("member_cnic_7").value ? document.getElementById("member_cnic_7").value : "Member 7 CNIC",
+        member7Institute: document.getElementById("member_institute_7").value ? document.getElementById("member_institute_7").value : "Member 7 Institute",
+        payment: document.getElementById("no_of_accommodations").value ? document.getElementById("no_of_accommodations").value : "payment"
+    };
+
+
+    const serviceID = "service_ccd3dws";
+    const templateID = "template_h96vrkv";
+
+    emailjs.send(serviceID, templateID, params)
+        .then((res) => {
+
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("cnic").value = "";
+            document.getElementById("contact").value = "";
+            document.getElementById("institute").value = "";
+            document.getElementById("leader_tag").value = "";
+            document.getElementById("dimension").value = "";
+            document.getElementById("weapon").value = "";
+            document.getElementById("team_name").value = "";
+            document.getElementById("reference_code").value = "";
+            document.getElementById("member_name_1").value = "";
+            document.getElementById("member_cnic_1").value = "";
+            document.getElementById("member_institute_1").value = "";
+            document.getElementById("member_name_2").value = "";
+            document.getElementById("member_cnic_2").value = "";
+            document.getElementById("member_institute_2").value = "";
+
+            document.getElementById("member_name_3").value = "";
+            document.getElementById("member_cnic_3").value = "";
+            document.getElementById("member_institute_3").value = "";
+
+            document.getElementById("member_name_4").value = "";
+            document.getElementById("member_cnic_4").value = "";
+            document.getElementById("member_institute_4").value = "";
+
+            document.getElementById("member_name_5").value = "";
+            document.getElementById("member_cnic_5").value = "";
+            document.getElementById("member_institute_5").value = "";
+
+            document.getElementById("member_name_6").value = "";
+            document.getElementById("member_cnic_6").value = "";
+            document.getElementById("member_institute_6").value = "";
+
+            document.getElementById("member_name_7").value = "";
+            document.getElementById("member_cnic_7").value = "";
+            document.getElementById("member_institute_7").value = "";
+
+            document.getElementById("no_of_accommodations").value = "";
+            document.getElementById("member_tag_1").value = "";
+            document.getElementById("member_tag_2").value = "";
+            document.getElementById("member_tag_3").value = "";
+            document.getElementById("member_tag_4").value = "";
+
+            console.log(res);
+            alert("Your Data is Collected Successfully...");
+        })
+        .catch((err) => console.log(err));
+}
+
---
+++ static/assets/js/counter.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function ($) {
+    var timerdate = "05/04/2024";
+
+	$("#countdown").countdown(timerdate, function(event) {
+        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
+    });
+
+	$("#countdown_reg").countdown(timerdate, function(event) {
+	    var daysMinusTwo = event.strftime("%D") - 2;
+        $(this).html(event.strftime("<div class='cd-item'><span>" + daysMinusTwo + "</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
+    });
+
+})(jQuery);
